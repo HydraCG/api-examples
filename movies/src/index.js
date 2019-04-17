5a19fb71e780fe361b830634110f0240255809d3
Angelo Veltens <angelo.veltens@codecentric.de>
2019-04-17 14:43:07 +0200

    basic entrypoint and context

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const entrypoint = require('./resources/entrypoint.json');
+const context = require('./resources/context.json');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  sendJsonLd(res, entrypoint);
 });
+
+app.get('/context.jsonld', function (req, res) {
+  sendJsonLd(res, context);
+});
+
+function sendJsonLd (res, resource) {
+  res.header("Content-Type", 'application/ld+json');
+  res.send(JSON.stringify(resource));
+}
 
 app.listen(3000, function () {
   console.log('Example API listening on port 3000!');
